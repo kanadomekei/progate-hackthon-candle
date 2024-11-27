4142b9e2f1f4c995142e4ae62114f7f8c6ba7422
kanadome <tsuchiyakeita2025@gmail.com>
2024-11-28 03:53:19 +0900

    cdを再確認

--- backend/app/main.go
+++ backend/app/main.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "hello world with test",
+			"message": "hello world from lambda",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
