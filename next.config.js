f92a0ffec2a1f88cf19d34c23ce9c3f7330bee1a
kanadome <tsuchiyakeita2025@gmail.com>
2024-12-06 00:39:11 +0900

    設定ファイルを追加

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/**',
+      },
+      // 本番環境のURLパターンも必要に応じて追加
+    ],
+  },
+}
+
+module.exports = nextConfig 
