4bf33c9bc7d5ee74da21ee1c76ea09ab5ec5ce2f
mizuki murata <mizucmke@gmail.com>
2024-12-06 23:53:37 +0900

    翻訳の追加

--- back-fastapi/app/main.py
+++ back-fastapi/app/main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from fastapi.responses import StreamingResponse
+from fastapi.responses import Response
 from dotenv import load_dotenv
+import httpx
 import base64
 import boto3
 import json
 import os
 import io
+import re
 
 # .envファイルの読み込み
 load_dotenv()
@@ -51,6 +54,12 @@ class ImageGenerationRequest(BaseModel):
     prompt: str
 
 
+# 日本語判別
+def translate_en(text: str) -> bool:
+    japanese_pattern = re.compile(r'[\u3040-\u30FF\u4E00-\u9FFF]')
+    return bool(japanese_pattern.search(text))
+
+
 class ImageGenerator:
     @staticmethod
     def decode_and_stream_response(model_response: dict) -> StreamingResponse:
@@ -77,11 +86,21 @@ class ImageGenerator:
 
 @app.post("/generate-image-stable-diffusion")
 async def generate_image_stable_diffusion(request_body: ImageGenerationRequest):
+    if translate_en(request_body.prompt):
+        async with httpx.AsyncClient() as client:
+            translate_response = await client.post(
+                "http://127.0.0.1:8000/translate-jp-to-en", 
+                json={"text": request_body.prompt}
+            )
+        translated_text = translate_response.text
+        request_body.prompt = translated_text
+        print(request_body)
     body = {"prompt": request_body.prompt, "mode": "text-to-image"}
 
     try:
         response = bedrock_runtime.invoke_model(modelId=MODELID, body=json.dumps(body))
         model_response = json.loads(response["body"].read())
+        print(model_response['seeds'])
         return ImageGenerator.decode_and_stream_response(model_response)
     except Exception as e:
         raise HTTPException(
@@ -91,6 +110,16 @@ async def generate_image_stable_diffusion(request_body: ImageGenerationRequest):
 
 @app.post("/generate-image-nova-canvas")
 async def generate_image_nova_canvas(request_body: ImageGenerationRequest):
+    if translate_en(request_body.prompt):
+        async with httpx.AsyncClient() as client:
+            translate_response = await client.post(
+                "http://127.0.0.1:8000/translate-jp-to-en", 
+                json={"text": request_body.prompt}
+            )
+        translated_text = translate_response.text
+        request_body.prompt = translated_text
+        print(f"print: {request_body}")
+        print(f"print: {request_body.prompt}")
     body = {
         "taskType": "TEXT_IMAGE",
         "textToImageParams": {"text": request_body.prompt},
@@ -98,10 +127,9 @@ async def generate_image_nova_canvas(request_body: ImageGenerationRequest):
             "width": 1024,
             "height": 1024,
             "quality": "standard",
-            "numberOfImages": 1,
+            "numberOfImages": 3,
         },
     }
-
     try:
         nova_canvas_client = ImageGenerator.create_bedrock_client("us-east-1")
         response = nova_canvas_client.invoke_model(
@@ -128,7 +156,23 @@ async def translate_jp_to_en(request_body: TranslationRequest):
             SourceLanguageCode='ja',
             TargetLanguageCode='en'
         )
-        return {"translated_text": response.get('TranslatedText')}
+        translated_text = response.get('TranslatedText', "")
+        return Response(content=translated_text, media_type="text/plain")
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Translation failed: {str(e)}"
+        )
+    
+
+def translate_jp_en(request_body: TranslationRequest):
+    translate_client = boto3.client('translate', region_name=REGION)
+    try:
+        response = translate_client.translate_text(
+            Text=request_body.text,
+            SourceLanguageCode='ja',
+            TargetLanguageCode='en'
+        )
+        return response.get('TranslatedText')
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Translation failed: {str(e)}"
