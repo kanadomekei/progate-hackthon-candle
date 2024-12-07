78884de1928ed7796153f77cabe6f1bb04daac14
mizuki murata <mizucmke@gmail.com>
2024-12-07 19:13:49 +0900

    画像修正の追加

--- back-fastapi/app/main.py
+++ back-fastapi/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 from fastapi.responses import StreamingResponse
 from fastapi.responses import Response
+from botocore.exceptions import ClientError
 from dotenv import load_dotenv
+from PIL import Image
+import requests
+import logging
 import httpx
 import base64
 import boto3
@@ -54,6 +58,11 @@ class ImageGenerationRequest(BaseModel):
     prompt: str
 
 
+class ImageGenerationRequestitoi(BaseModel):
+    prompt: str
+    image_url: str
+
+
 # 日本語判別
 def translate_en(text: str) -> bool:
     japanese_pattern = re.compile(r'[\u3040-\u30FF\u4E00-\u9FFF]')
@@ -82,6 +91,8 @@ class ImageGenerator:
             return boto3.client("bedrock-runtime", region_name=region)
         except Exception as e:
             raise RuntimeError(f"Failed to initialize Bedrock client: {e}")
+        
+        
 
 
 @app.post("/generate-image-stable-diffusion")
@@ -141,6 +152,107 @@ async def generate_image_nova_canvas(request_body: ImageGenerationRequest):
         raise HTTPException(
             status_code=500, detail=f"Image generation failed: {str(e)}"
         )
+
+# ここから画像修正
+class ImageError(Exception):
+    "Custom exception for errors returned by SDXL"
+    def __init__(self, message):
+        self.message = message
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def resize_image_to_limit(image: Image, max_pixels: int = 1048576) -> Image:
+    """
+    Resize the image to ensure it is within the max pixel limit.
+    
+    Args:
+        image_path (str): Path to the input image.
+        max_pixels (int): Maximum allowed pixel count.
+    
+    Returns:
+        PIL.Image: Resized image object.
+    """
+    # 画像をPillowで開く
+    image = Image.open(image)
+
+    width, height = image.size
+    
+    current_pixels = width * height
+
+    if current_pixels <= max_pixels:
+        return image
+    
+    scale_factor = (max_pixels / current_pixels) ** 0.5
+    new_width = int(width * scale_factor)
+    new_height = int(height * scale_factor)
+    resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)    
+    return resized_image
+
+    
+def generate_image(model_Id, body):
+    logger.info("Generating image with SDXL model %s", model_Id)
+
+    bedrock = boto3.client(service_name='bedrock-runtime')
+
+    accept = "application/json"
+    content_type = "application/json"
+
+    response = bedrock.invoke_model(
+        body=body, modelId=model_Id, accept=accept, contentType=content_type
+    )
+    response_body = json.loads(response.get("body").read())
+
+    finish_reason = response_body.get("artifacts")[0].get("finishReason")
+    if finish_reason == 'ERROR' or finish_reason == 'CONTENT_FILTERED':
+        raise ImageError(f"Image generation error. Error code is {finish_reason}")
+
+    base64_image = response_body.get("artifacts")[0].get("base64")
+    logger.info("Successfully generated image with the SDXL model %s", model_Id)
+
+    return {"images": [base64_image]}
+
+
+# stable-diffusionのID
+model_Id = 'stability.stable-diffusion-xl-v1'
+# stable-diffusionで画像修正
+@app.post("/generate-image-to-image-stable-diffusion")
+async def generate_image_stable_diffusion(request_body: ImageGenerationRequestitoi):
+    if translate_en(request_body.prompt):
+        async with httpx.AsyncClient() as client:
+            translate_response = await client.post(
+                "http://127.0.0.1:8000/translate-jp-to-en", 
+                json={"text": request_body.prompt}
+            )
+        translated_text = translate_response.text
+        request_body.prompt = translated_text
+
+    try:
+        response = requests.get(request_body.image_url)
+        response.raise_for_status()
+        init_image = base64.b64encode(response.content).decode('utf8')
+    except requests.RequestException as e:
+        raise HTTPException(status_code=400, detail=f"Failed to fetch image: {e}")
+
+    body = json.dumps({
+        "text_prompts": [{"text": request_body.prompt}],
+        "init_image": init_image,
+        "style_preset": "isometric"
+    })
+
+    try:
+        model_response = generate_image(model_Id=model_Id, body=body)
+        return ImageGenerator.decode_and_stream_response(model_response)
+    except ClientError as err:
+        message = err.response["Error"]["Message"]
+        logger.error("A client error occurred: %s", message)
+        raise HTTPException(status_code=500, detail=f"Client error: {message}")
+    except ImageError as err:
+        logger.error(err.message)
+        raise HTTPException(status_code=500, detail=err.message)
+
 
 
 class TranslationRequest(BaseModel):
